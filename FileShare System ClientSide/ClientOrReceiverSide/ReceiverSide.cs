b431a55a51921e32bd40261ebae9304dd0ca26ce
Spec?? <salmantariq8385@gmail.com>
2022-10-28 18:03:42 +0500

    First initial push for GitHub Add project files.

---
+++ FileShare System ClientSide/ClientOrReceiverSide/ReceiverSide.cs
@@ -0,0 +1,14 @@
+﻿namespace FileShare_System_ClientSide.ClientOrReceiverSide
+{
+    internal static class ReceiverSide
+    {
+        public static void Main(string[] args)
+        {
+            ExecuteClient();
+        }
+
+        private static void ExecuteClient()
+        {
+        }
+    }
+}
---
+++ FileShare system/SenderOrServerSide/SenderSide.cs
@@ -0,0 +1,29 @@
+﻿using System.Net;
+using System.Net.Sockets;
+
+namespace FileShare_system.SenderOrServerSide
+{
+    public static class SenderSide
+    {
+        private class severTCP
+        {
+        }
+
+        public static void Main(string[] args)
+        {
+            ExecuteServer();
+        }
+
+        private static void establishConnection()
+        {
+            IPEndPoint ep = new IPEndPoint(host, 1234); // Address
+            TcpListener listener = new TcpListener(ep); // Instantiate the object
+            listener.Start();// Start listening...
+        }
+
+        private static void ExecuteServer()
+        {
+            establishConnection();
+        }
+    }
+}
